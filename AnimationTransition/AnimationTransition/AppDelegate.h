046d8749da945d5ec403ab3e9ab06a5d7fcccc84
Genglei <1247759034@qq.com>
2016-03-23 17:04:55 +0800

    add Xcode

---
+++ AnimationTransition/AnimationTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTransition
+//
+//  Created by geng lei on 16/3/23.
+//  Copyright © 2016年 com.fengche.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationTransition/AnimationTransition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationTransition
+//
+//  Created by geng lei on 16/3/23.
+//  Copyright © 2016年 com.fengche.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
